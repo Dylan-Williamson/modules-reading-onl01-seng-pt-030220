00b8e37886b1d9ae71adf7600a3919d270f9526e
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-04 15:18:58 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,4 +5,9 @@ require_relative './fancy_dance.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  include Dance 
+  
+  def initialize(name)
+    @name = name
+  end
 end
